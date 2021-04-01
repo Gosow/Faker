c0f33c62a7eb873709e37b1ed07ce73c04a211c1
Alkassoum Yacine <43809571+Gosow@users.noreply.github.com>
2021-04-01 16:28:46 +0200

    Update flask_main.py

--- flask_main.py
+++ flask_main.py
@@ -2,64 +2,142 @@ import numpy as np
 from flask import Flask, abort, jsonify, request,render_template
 import pickle
 import json #ajouter dans requirements
-
+import helpers as utilss
+from textblob import TextBlob
+from flask_mail import Mail, Message
 my_model = pickle.load(open("./Faker_serialized.pkl","rb"))
-
+stopwords = pickle.load(open("./Faker_stopwords_en","rb"))               
 app = Flask(__name__)
+mail= Mail(app)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'enteryourmail@gmail.com'
+app.config['MAIL_PASSWORD'] = 'enteryourpassword'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+
+
+@app.route("/send",methods =['POST'])
+def index_mail():
+        file = request.get_json(force = True)
+        file = file['form']
+
+        msg = Message('NEW', sender = 'FakerNewsmails@gmail.com', recipients = ['FakerNewsmails@gmail.com'])
+        msg.body = file
+        mail.send(msg)
+        return "Sent"
+
 @app.route('/',methods = ['GET'])
 def index():
         return render_template('index.html')
 @app.route('/predicttxt', methods=['POST'])
 def classify():
         if request.method == 'POST':
+                punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
                 
                 file = request.form['article']
-                #print(file)
-                #data = request.get_json(force = True)
+                for ele in file:  
+                        if ele in punc:  
+                                file = file.replace(ele, " ")
+                
                 predict_request = [file]
+                predict_request = [i.split() for i in predict_request]
+                print(predict_request)
+
+                predict_request= [' '.join([word for word in predict_request[0] if not word in list(stopwords)])]
+                analysis = TextBlob(str(predict_request))
+
+
+
                 predict_request = np.array(predict_request)
                 percent,y_hat = my_model.predict_proba(predict_request),my_model.predict(predict_request)
-                #10 mots qui nous ont le plus fait tiltés
-                predict_request = [i.split() for i in predict_request]
+             
+                predict_request = [i.split() for i in predict_request][0]
+                print(predict_request)
                 pro=[]
-                dicts = np.array([])
+                dicts =[]
                 pr=[]
+                dictio = {}
+                
+              
                 for e in predict_request:
-                        pr = my_model.predict_proba(e)
+                        pr = my_model.predict_proba([e])
+                        #print(my_model.predict_proba([e]))
                         pro.append(pr)
-                        #dicts[e] = [pr]
-
-                #pro = [x for x in pro]
-                #pro.tolist()
-                
-                #final = json.dumps(pro)
-                #dict(zip(predict_request,pro))
-                #print(list(zip(predict_request,pro[0])))
-                
-
-
-
-                
+                        dicts.append(e)
+                        if e not in dictio:
+                                dictio[e] = 0 
+                        dictio[e] += 1
+            
                 pro = pro[0].tolist()
+                
                 return jsonify({
+                        
                         'type' : y_hat[0],
                         'proba_false':percent[0][0],
                         'proba_true':percent[0][1],
-                        'Tilt': pro
+                        'polarity':analysis.sentiment.polarity,
+                        'subjectivity':analysis.sentiment.subjectivity,
+                        'Mots':dictio
 
                 })
                  
 @app.route('/predictapi', methods=['POST'])
 def classifyapi():
         if request.method == 'POST':
-                data = request.get_json(force = True)
-                predict_request = [data['article']]
+               
+                punc = '''!()-[]{};:'"\, <>./?@#$%^&*_“”‘’~'''
+                
+                file = request.get_json(force = True)
+                file = file['article']
+                
+
+                for ele in file:  
+                        if ele in punc:  
+                                file = file.replace(ele, " ")
+
+                predict_request = [file]
+                predict_request = [i.split() for i in predict_request]
+                
+               
+                predict_request= [' '.join([word for word in predict_request[0] if not word in list(stopwords)])]
+                lang=TextBlob(file).detect_language()
+                analysis = TextBlob(str(predict_request))
+             
+
                 predict_request = np.array(predict_request)
                 percent,y_hat = my_model.predict_proba(predict_request),my_model.predict(predict_request)
+
+                
+          
+                
+                predict_request = [i.split() for i in predict_request][0]
+                pro=[]
+                dicts =[]
+                pr=[]
+                dictio = {}
+                
+
+                for e in predict_request:
+                        pr = my_model.predict_proba([e])
+                        pro.append(pr)
+                        dicts.append(e)
+                        if e not in dictio:
+                                dictio[e] = 0 
+                        dictio[e] += 1
+          
+                pro = pro[0].tolist()
+                dictio=utilss.hdict(dictio,15)
                 return jsonify({
+                        'language':lang,
                         'type' : y_hat[0],
-                        'proba_false':percent[0][0],
-                        'proba_true':percent[0][1]
+                        'proba_false':np.round((percent[0][0])*100),
+                        'proba_true':np.round(100*(percent[0][1])),
+                        'polarity':np.round(100*analysis.sentiment.polarity),
+                        'subjectivity':np.round(100*analysis.sentiment.subjectivity),
+                        'Mots': dictio
 
                 })
 if __name__ == '__main__':
